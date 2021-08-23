b683b11a35cfa0043b0d09ae1347882339ce39d1
Abhi Rana <63769307+Avon11@users.noreply.github.com>
2021-08-23 12:45:26 +0530

    Add files via upload

---
+++ browser.py
@@ -0,0 +1,57 @@
+# pip install PyQt5
+# pip install PyQtWebEngine
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # Navbar
+        navigation_bar = QToolBar()
+        self.addToolBar(navigation_bar)
+
+        back_button = QAction('Back', self)
+        back_button.triggered.connect(self.browser.back)
+        navigation_bar.addAction(back_button)
+
+        forward_button = QAction('Forward', self)
+        forward_button.triggered.connect(self.browser.forward)
+        navigation_bar.addAction(forward_button)
+
+        reload_button = QAction('Reload', self)
+        reload_button.triggered.connect(self.browser.reload)
+        navigation_bar.addAction(reload_button)
+
+        home_button = QAction('Home', self)
+        home_button.triggered.connect(self.navigation_home)
+        navigation_bar.addAction(home_button)
+        self.url_bar = QLineEdit()
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    
+    def navigation_home(self):
+        self.browser.setUrl(QUrl('http://google.com'))
+
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName(" KENOBI⭐ ")
+main = Window()
+app.exec_()
